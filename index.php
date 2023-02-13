9d1f87cb216a3823caee9ee01baeb654a7b0193c
Abdulnabi Shaikh <abdulnabishaikh6545@eng.rizvi.edu.in>
2023-02-14 00:54:44 +0530

    News application

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express News</title>
+
+    <style>
+        .news{
+            margin-left: 4pc;
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .image{
+            border: 2px solid grey;
+            border-radius: 10px;
+            width: 400px;
+        }
+    </style>
+</head>
+<body>
+
+
+        <!-- <select id="" style="width: 100%;">
+        <option value="">Select One</option>
+        <option id="source" onclick="">The Times Of India</option>
+        </select> -->
+
+    <div class="heading">
+        <h1>
+            <center>Express News</center> 
+        </h1>
+        <h2>
+            <center>Latest Coverage..........</center> 
+        </h2>
+    </div>
+
+
+    <div class="news" id="newsA">
+        <!-- <div class="image">
+            <img src="" alt="">
+            <div class="title">
+                <button onclick="">
+
+                </button>
+                <div class="content">
+
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <script src="indesx.js"></script>
+    <!-- <script src="indesx.js"></script> -->
+</body>
+</html>
+
